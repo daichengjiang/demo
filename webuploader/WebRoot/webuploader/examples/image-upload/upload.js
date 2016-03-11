0b3bcc30c8a5bc4146fffdb50b88744b02434070
daichengjiang <13086623773@163.com>
2016-03-11 13:59:59 +0800

    优化上传操作及实现暂停/继续上传功能
    Signed-off-by: daichengjiang <13086623773@163.com>

--- webuploader/WebRoot/webuploader/examples/image-upload/upload.js
+++ webuploader/WebRoot/webuploader/examples/image-upload/upload.js
@@ -251,7 +251,7 @@
                 uploader.makeThumb( file, function( error, src ) {
                 	
                 	if(file.type.match('image/') == null){
-                		$wrap.text( '压缩文件不能预览' );
+                		$wrap.text( '非图片文件不能预览' );
                 		return;
                 	}
                 	
@@ -375,52 +375,51 @@
             $li.off().find('.file-panel').off().end().remove();
         }
 
+        //更新总进度
         function updateTotalProgress() {
             var loaded = 0,
                 total = 0,
                 spans = $progress.children(),
                 percent;
-
             $.each( percentages, function( k, v ) {
                 total += v[ 0 ];
                 loaded += v[ 0 ] * v[ 1 ];
             } );
-
             percent = total ? loaded / total : 0;
-
-
             spans.eq( 0 ).text( Math.round( percent * 100 ) + '%' );
             spans.eq( 1 ).css( 'width', Math.round( percent * 100 ) + '%' );
             updateStatus();
         }
 
+        //更新当前状态红色提示
         function updateStatus() {
             var text = '', stats;
 
             if ( state === 'ready' ) {
-                text = '选中' + fileCount + '张图片，共' +
+                text = '选中' + fileCount + '份文件，共' +
                         WebUploader.formatSize( fileSize ) + '。';
             } else if ( state === 'confirm' ) {
                 stats = uploader.getStats();
                 if ( stats.uploadFailNum ) {
-                    text = '已成功上传' + stats.successNum+ '张照片至XX相册，'+
-                        stats.uploadFailNum + '张照片上传失败，<a class="retry" href="#">重新上传</a>失败图片或<a class="ignore" href="#">忽略</a>'
+                    text = '已成功上传' + stats.successNum+ '份文件，'+
+                        stats.uploadFailNum + '份文件上传失败，<a class="retry" href="#">重新上传</a>失败文件或<a class="ignore" href="#">忽略</a>'
                 }
-
+                
             } else {
                 stats = uploader.getStats();
-                text = '共' + fileCount + '张（' +
+                text = '共' + fileCount + '份（' +
                         WebUploader.formatSize( fileSize )  +
-                        '），已上传' + stats.successNum + '张';
+                        '），已上传' + stats.successNum + '份';
 
                 if ( stats.uploadFailNum ) {
-                    text += '，失败' + stats.uploadFailNum + '张';
+                    text += '，失败' + stats.uploadFailNum + '份';
                 }
             }
 
             $info.html( text );
         }
 
+        //设置上传状态并更改按钮名称
         function setState( val ) {
             var file, stats;
 
@@ -485,6 +484,81 @@
             updateStatus();
         }
 
+        /**
+         * 扩展md5逻辑
+         * method:before-send-file
+         * 在文件发送之前request，此时还没有分片（如果配置了分片的话），可以用来做文件整体md5验证。
+         * para:file: File对象
+         *//*
+        uploader.register({
+            'before-send-file' : 'preupload'
+        }, {
+            preupload : function(file) {
+                $('#uploadBtn').attr("disabled",true);
+                var me = this, 
+                owner = this.owner, 
+                server = me.options.server, 
+                deferred = WebUploader.Deferred(), 
+                blob = file.source.getSource();
+                //var fileMd5 = file.wholeMd5;
+                var start =  +new Date();
+                var $li = $('#' + file.id), 
+                $MD5Percent = $li.find('.progress  .progress-bar-info');
+                $li.find('span.state').text('计算MD5');
+                insertLog("<br>"+moment().format("YYYY-MM-DD HH:mm:ss")+" before-send-file  preupload:开始计算文件("+file.name+")MD5. ");
+                owner.md5File( file )           
+                .progress(function(percentage) {   // 及时显示进度             
+                    var percent = parseInt(percentage * 100 ) + '%';
+                    $MD5Percent.css('width', percent);
+                    $MD5Percent.html(percent);
+                    //console.log('Percentage:', percent);
+                })               
+                .then(function(fileMd5) {   // 完成              
+                    var end = +new Date();
+                   // console.log("before-send-file  preupload: file.size="+file.size+" file.md5="+fileMd5);
+                    insertLog("<br>"+moment().format("YYYY-MM-DD HH:mm:ss")+" before-send-file  preupload:计算文件("+file.name+")MD5完成. 耗时  " + (end - start) + '毫秒  fileMd5: ' + fileMd5);
+                    file.wholeMd5 = fileMd5;
+                    $.ajax({
+                        cache : false,                  
+                        type : "post",
+                        dataType : "json",
+                        url : baseUrl + "/fileUpload/existsMd5",
+                        data : {
+                            fileMd5 : fileMd5,
+                            fileName : file.name,
+                            isShared : $("#isShared").val()
+                        },
+                        success : function(result) {
+                                $('#uploadBtn').attr("disabled",false);
+                                me.options.formData.fileMd5 = fileMd5;
+                                me.options.formData.isShared = $("#isShared").val();
+                                me.options.formData.fileType = $("#fileType").val();
+                                if (result.result) {//文件存在
+                                    insertLog("<br>"+moment().format("YYYY-MM-DD HH:mm:ss")+" before-send-file  preupload:文件 "+file.name + " 已经存在，跳过上传   fileMd5:"+fileMd5);
+                                    $MD5Percent.hide();
+                                    var $li = $('#' + file.id), 
+                                    $percent = $li.find('.progress  .progress-bar-success');
+                                    $li.find('span.state').text('文件重复，已跳过');
+                                    $percent.css('width', 100 + '%');
+                                    owner.skipFile(file);
+                                }else{//文件不存在
+                                    file.wholeMd5 = fileMd5;
+                                    file.chunkMd5s = result.chunkMd5s;  //如果后台已经有该文件的分片记录
+                                }
+                                // me.data.chunksMd5 = "chunksMd5";
+                                $('#' + file.id+' .cancleBtn').removeClass("btn-info");
+                                $('#' + file.id+' .cancleBtn').attr("disabled",true);
+                                deferred.resolve(true);
+                                // return deferred.reject();
+                        }
+                    });
+                });
+                return deferred.promise();
+            }
+        });*/
+        
+        
+        
         uploader.onUploadProgress = function( file, percentage ) {
             var $li = $('#'+file.id),
                 $percent = $li.find('.progress span');
@@ -543,17 +617,29 @@
             alert( 'Eroor: ' + code );
         };
 
+        /**
+         * 注册上传按钮点击事件
+         * ready：准备就绪，按钮操作（开始上传）
+         * uploading：上传中，按钮操作（暂停上传）
+         * paused：已暂停，按钮操作（继续上传）
+         */
         $upload.on('click', function() {
             if ( $(this).hasClass( 'disabled' ) ) {
                 return false;
             }
-
             if ( state === 'ready' ) {
                 uploader.upload();
             } else if ( state === 'paused' ) {
                 uploader.upload();
             } else if ( state === 'uploading' ) {
-                uploader.stop();
+            	
+            	/**
+            	 * 暂停上传，可传递一个参数
+            	 * stop() 暂停无效
+            	 * stop(true) 暂停正在上传操作
+            	 * stop(file) 暂停指定文件上传
+            	 */
+                uploader.stop(true);
             }
         });
 
--- webuploader/src/com/webuploader/servlet/UpLoadServlet.java
+++ webuploader/src/com/webuploader/servlet/UpLoadServlet.java
@@ -18,6 +18,7 @@ import org.apache.commons.fileupload.servlet.ServletFileUpload;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
 
 import com.blogme.taylor.RMI.UploadFileRmi;
 import com.blogme.taylor.RMI.params.FileInformation;
@@ -44,8 +45,7 @@ import com.sun.jersey.api.core.HttpContext;
 public class UpLoadServlet extends HttpServlet {  
 	private static final long serialVersionUID = 903713247645421461L;
 
-	public void doPost(HttpServletRequest request, HttpServletResponse response)  
-            throws ServletException, IOException {  
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {  
           
         request.setCharacterEncoding("utf-8");  //设置编码  
           
@@ -53,185 +53,121 @@ public class UpLoadServlet extends HttpServlet {
         DiskFileItemFactory factory = new DiskFileItemFactory();  
         //获取文件需要上传到的路径  
         String path = request.getRealPath("/upload");  
-        //如果没以下两行设置的话，上传大的 文件 会占用 很多内存，  
-        //设置暂时存放的 存储室 , 这个存储室，可以和 最终存储文件 的目录不同  
-        /** 
-         * 原理 它是先存到 暂时存储室，然后在真正写到 对应目录的硬盘上，  
-         * 按理来说 当上传一个文件时，其实是上传了两份，第一个是以 .tem 格式的  
-         * 然后再将其真正写到 对应目录的硬盘上 
-         */  
         try {
+        	  String tp =   path+File.separator+new Date().getTime();
+        	  Object attribute = request.getSession().getAttribute("tempDIR");
+        	  if(attribute==null){
+        		  request.getSession().setAttribute("tempDIR",tp);
+        	  }else{
+        		  tp = (String) attribute;
+        	  }
+        	File  tpfFile = new File(tp);
+        	if(!tpfFile.exists()){
+        		//  FileUtils.deleteDirectory(tpfFile);
+        		tpfFile.mkdirs();
+        	}
+        	//如果没以下两行设置的话，上传大的 文件 会占用 很多内存，  
+            //设置暂时存放的 存储室 , 这个存储室，可以和 最终存储文件 的目录不同  
+            /** 
+             * 原理 它是先存到 暂时存储室，然后在真正写到 对应目录的硬盘上，  
+             * 按理来说 当上传一个文件时，其实是上传了两份，第一个是以 .tem 格式的  
+             * 然后再将其真正写到 对应目录的硬盘上 
+             */  
+        	factory.setRepository(tpfFile);  
+        	//设置 缓存的大小，当上传文件的容量超过该缓存时，直接放到 暂时存储室  
+        	factory.setSizeThreshold(1024*1024) ;  
+        	//高水平的API文件上传处理  
+        	ServletFileUpload upload = new ServletFileUpload(factory);  
+            // 得到所有的表单域，它们目前都被当作FileItem
+            List<FileItem> fileItems = upload.parseRequest(request);
+            //上传文件名
+            String fileName = "";
+            //标识唯一的id
+            String guid = "";
+            // 总分片数
+            int chunks = 0;
+            //标识当前分片在上传分片中的顺序（从0开始）
+            int chunk = 0;
+            FileItem tempFileItem = null;
+            //遍历获取表单域中的部分值
+            for (FileItem fileItem : fileItems) {
+                if (fileItem.getFieldName().equals("guid")) {
+                    guid = fileItem.getString();
+                } else if (fileItem.getFieldName().equals("name")) {
+                    fileName = new String(fileItem.getString().getBytes("ISO-8859-1"), "UTF-8");
+                } else if (fileItem.getFieldName().equals("chunks")) {
+                    chunks = Integer.parseInt(fileItem.getString());
+                } else if (fileItem.getFieldName().equals("chunk")) {
+                    chunk = Integer.parseInt(fileItem.getString());
+                } else if (fileItem.getFieldName().equals("file")) {
+                    tempFileItem = fileItem;
+                }
+            }
             boolean isMultipart = ServletFileUpload.isMultipartContent(request);
- 
+            //判断是否为上传文件
             if (isMultipart) {
-            	String tp =   path+File.separator+new Date().getTime();
-            	  Object attribute = request.getSession().getAttribute("tempDIR");
-            	  if(attribute==null){
-            		  request.getSession().setAttribute("tempDIR",tp);
-            	  }else{
-            		  tp = (String) attribute;
-            	  }
-            	File  tpfFile = new File(tp);
-            	if(!tpfFile.exists()){
-            		//  FileUtils.deleteDirectory(tpfFile);
-            		tpfFile.mkdirs();
-            	}
-            	
-            	
-            	factory.setRepository(tpfFile);  
-            	//设置 缓存的大小，当上传文件的容量超过该缓存时，直接放到 暂时存储室  
-            	factory.setSizeThreshold(1024*1024) ;  
-            	//高水平的API文件上传处理  
-            	ServletFileUpload upload = new ServletFileUpload(factory);  
-                // 得到所有的表单域，它们目前都被当作FileItem
-                List<FileItem> fileItems = upload.parseRequest(request);
-                //上传文件名
-                String fileName = "";
-                //标识唯一的id
-                String guid = "";
-                // 总分片数
-                int chunks = 0;
-                //标识当前分片在上传分片中的顺序（从0开始）
-                int chunk = 0;
-                FileItem tempFileItem = null;
-                //遍历获取表单域中的部分值
-                for (FileItem fileItem : fileItems) {
-                    if (fileItem.getFieldName().equals("guid")) {
-                        guid = fileItem.getString();
-                    } else if (fileItem.getFieldName().equals("name")) {
-                        fileName = new String(fileItem.getString().getBytes("ISO-8859-1"), "UTF-8");
-                    } else if (fileItem.getFieldName().equals("chunks")) {
-                        chunks = Integer.parseInt(fileItem.getString());
-                    } else if (fileItem.getFieldName().equals("chunk")) {
-                        chunk = Integer.parseInt(fileItem.getString());
-                    } else if (fileItem.getFieldName().equals("file")) {
-                        tempFileItem = fileItem;
+            	//判断是否为分片上传，当总分片数大于1时，表示是分片上传
+            	if (chunks > 1) {
+            		// 临时目录用来存放所有分片文件
+                    String tempFileDir = path + File.separator + "part";
+                    File parentFileDir = new File(tempFileDir);
+                    if (!parentFileDir.exists()) {
+                    	   parentFileDir.mkdirs();
                     }
-                }
-                // 临时目录用来存放所有分片文件
-                String tempFileDir = path + File.separator + "part";
-                File parentFileDir = new File(tempFileDir);
-                if (!parentFileDir.exists()) {
-                	   parentFileDir.mkdirs();
-                }
- 
-                // 分片处理时，前台会多次调用上传接口，每次都会上传文件的一部分到后台(默认每片为5M)
-                File tempPartFile = new File(parentFileDir, guid + "_" + chunk + ".part");
-                FileUtils.copyInputStreamToFile(tempFileItem.getInputStream(),tempPartFile);
-                
-                // 是否全部上传完成
-                // 所有分片都存在才说明整个文件上传完成
-                boolean uploadDone = true;
-                for (int i = 0; i < chunks; i++) {
-                    File partFile = new File(parentFileDir, guid + "_" + i + ".part");
-                    if (!partFile.exists()) {
-                        uploadDone = false;
-                    }
-                }
-                // 所有分片文件都上传完成
-                // 将所有分片文件合并到一个文件中
-                if (uploadDone) {
-                    File destTempFile = new File(path, fileName);
+                    // 分片处理时，前台会多次调用上传接口，每次都会上传文件的一部分到后台(默认每片为5M)
+                    File tempPartFile = new File(parentFileDir, guid + "_" + chunk + ".part");
+                    FileUtils.copyInputStreamToFile(tempFileItem.getInputStream(),tempPartFile);
+                    // 是否全部上传完成
+                    // 所有分片都存在才说明整个文件上传完成
+                    boolean uploadDone = true;
                     for (int i = 0; i < chunks; i++) {
                         File partFile = new File(parentFileDir, guid + "_" + i + ".part");
-                        
-                        FileOutputStream destTempfos = new FileOutputStream(destTempFile, true);
- 
-                        FileUtils.copyFile(partFile, destTempfos);
- 
-                        destTempfos.close();
+                        if (!partFile.exists()) {
+                            uploadDone = false;
+                        }
                     }
-                    // 得到 destTempFile 就是最终的文件
-                    // 添加到文件系统或者存储中
-                     
-              	  // 删除临时目录中的分片文件
-                    FileUtils.deleteDirectory(parentFileDir);
-                    // 删除临时文件
-                    FileUtils.deleteDirectory(tpfFile);
-                    request.getSession().removeAttribute("tempDIR");
-                } else {
-                    // 临时文件创建失败
-                    if (chunk == chunks -1) {
+                    // 所有分片文件都上传完成
+                    // 将所有分片文件合并到一个文件中
+                    if (uploadDone) {
+                        File destTempFile = new File(path, fileName);
+                        for (int i = 0; i < chunks; i++) {
+                            File partFile = new File(parentFileDir, guid + "_" + i + ".part");
+                            
+                            FileOutputStream destTempfos = new FileOutputStream(destTempFile, true);
+     
+                            FileUtils.copyFile(partFile, destTempfos);
+     
+                            destTempfos.close();
+                        }
+                  	  	// 删除临时目录中的分片文件
                         FileUtils.deleteDirectory(parentFileDir);
+                        // 删除临时文件目录
+                        FileUtils.deleteDirectory(tpfFile);
+                        request.getSession().removeAttribute("tempDIR");
+                    } else {
+                        // 临时文件创建失败
+                        if (chunk == chunks -1) {
+                            FileUtils.deleteDirectory(parentFileDir);
+                        }
                     }
-                }
+				}else {
+	                  OutputStream out = new FileOutputStream(new File(path,fileName));  
+	                  InputStream in = tempFileItem.getInputStream();
+	                  int length = 0 ;  
+	                  byte [] buf = new byte[1024] ;  
+	                  // in.read(buf) 每次读到的数据存放在   buf 数组中  
+	                  while( (length = in.read(buf) ) != -1){  
+	                      //在   buf 数组中 取出数据 写到 （输出流）磁盘上  
+	                      out.write(buf, 0, length);  
+	                  }  
+	                  // 删除临时文件目录
+                      FileUtils.deleteDirectory(tpfFile);
+	                  in.close();  
+	                  out.close();
+				}
             }
         } catch (Exception e) {
         	e.printStackTrace();
         }
-  /*      try {  
-            //可以上传多个文件  
-            List<FileItem> list = (List<FileItem>)upload.parseRequest(request);  
-            int i = 0;
-            for(FileItem item : list)  
-            {  
-                //获取表单的属性名字  
-                String name = item.getFieldName();  
-                //如果获取的 表单信息是普通的 文本 信息  
-                if(item.isFormField())  
-                {                     
-                    //获取用户具体输入的字符串 ，名字起得挺好，因为表单提交过来的是 字符串类型的  
-                    String value = item.getString() ;  
-                    request.setAttribute(name, value);  
-                }  
-                //对传入的非 简单的字符串进行处理 ，比如说二进制的 图片，电影这些  
-                else  
-                {  
-                    *//** 
-                     * 以下三步，主要获取 上传文件的名字 
-                     *//*  
-                    //获取路径名  
-                    String value = item.getName() ;  
-                    //索引到最后一个反斜杠  
-                    int start = value.lastIndexOf("\\");  
-                    //截取 上传文件的 字符串名字，加1是 去掉反斜杠，  
-                    String filename = value.substring(start+1);  
-                    String extension = FilenameUtils.getExtension(filename);
-                    String baseName = FilenameUtils.getBaseName(filename);
-                    filename  =baseName +(i++)+"."+extension;
-                    request.setAttribute(name, filename);  
-                    //真正写到磁盘上  
-                    //它抛出的异常 用exception 捕捉  
-                    //item.write( new File(path,filename) );//第三方提供的
-                    
-                    //手动写的  
-                    OutputStream out = new FileOutputStream(new File(path,filename));  
-                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
-                    
-                    InputStream in = item.getInputStream() ;  
-                    int length = 0 ;  
-                    byte [] buf = new byte[1024] ;  
-                    System.out.println("获取上传文件的总共的容量："+item.getSize());  
-                    // in.read(buf) 每次读到的数据存放在   buf 数组中  
-                    while( (length = in.read(buf) ) != -1)  
-                    {  
-                        //在   buf 数组中 取出数据 写到 （输出流）磁盘上  
-                        out.write(buf, 0, length);  
-                    }  
-                    in.close();  
-                    out.close();
-                    
-                    
-                    bos.close();  
-                    byte[] bs = bos.toByteArray();
-                     
-                    UploadFileRmi  upload0 = (UploadFileRmi)Naming.lookup("rmi://172.18.2.114:9998/UploadFileRmi");  
-         			FileInformation  finf =  new FileInformationSev();
-         			
-         			finf.setInformation(filename, bs );
-        			
-        			upload0.uploadFile(finf);
-                }  
-            }  
-        } catch (FileUploadException e) {  
-            // TODO Auto-generated catch block  
-            e.printStackTrace();  
-        }  
-        catch (Exception e) {  
-            // TODO Auto-generated catch block  
-            e.printStackTrace();  
-        }  */
-        request.getRequestDispatcher("index.jsp").forward(request, response);  
     }  
-  
 }  
